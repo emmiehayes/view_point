b576788dfb31cda23adfdba75ce8fce9bf422f77
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-03 12:09:22 -0600

    Build out api connection to retreive movies

--- app/presenters/movie_search_presenter.rb
+++ app/presenters/movie_search_presenter.rb
@@ -1,10 +1,19 @@
 class MovieSearchPresenter 
 
-  def initialize(params)
-    @params = params
+  def initialize(movie)
+    @movie = movie
   end
 
   def movies 
-    
+    @conn = Faraday.new(url: 'http://tastedive.com') do |faraday|
+    faraday.adapter Faraday.default_adapter
+  end
+    response = @conn.get("/api/similar?q=#{@movie}&k=#{ENV["TASTEDIVE_API_KEY"]}&verbose=1&info")
+    search_movie = JSON.parse(response.body, symbolize_names: true)[:similar][:info]
+    results = JSON.parse(response.body, symbolize_names: true)[:results]
+
+    @movies  = results.map do |result|
+      Movie.new(result)
+    end
   end
 end
