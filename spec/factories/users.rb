f2ba20d8fcbb11ca5de5e7659b0aa8dd201d1a43
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-02 19:20:27 -0600

    Create User model and run migration

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    provider { "MyString" }
+    uid { "MyString" }
+    email { "MyString" }
+    first_name { "MyString" }
+    token { "MyString" }
+    refresh_token { "MyString" }
+    oauth_expires_at { "2018-11-02 19:14:08" }
+  end
+end
