a2d83e7f7f4318a119599f2b0cf8a5d6b44a3fd4
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-03 15:51:53 -0600

    Add check for current_user and test edge cases

--- spec/features/user_search_movie_recs_spec.rb
+++ spec/features/user_search_movie_recs_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 describe 'An authenticated user' do
   scenario 'can successfully search for movie recommendations' do
-
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     
     movie = "Interstellar"
-
     json_response = File.open('./fixtures/movie_search.json')
     stub_request(:get, "https://tastedive.com/api/similar?q=#{movie}&k=#{ENV['TASTEDIVE_API_KEY']}&verbose=1&info").to_return(status: 200, body: json_response)
 
@@ -17,7 +15,6 @@ describe 'An authenticated user' do
     
     expect(current_path).to eq('/search')
     expect(page).to have_css(".movie", count: 20)
-
     within(first(".movie")) do
       expect(page).to have_css(".title")
       expect(page).to have_css(".summary")
@@ -25,3 +22,19 @@ describe 'An authenticated user' do
     end
   end
 end
+
+describe 'A unauthenticated visitor' do
+  scenario 'cannot access the user dashboard' do
+    visit dashboard_path
+    expect(current_path).to eq('/')
+    expect(page).to have_button('Sign in with Google')
+    expect(page).to_not have_button('Sign Out')
+  end
+   scenario 'cannot access user search' do
+    visit search_path
+    expect(current_path).to eq('/')
+    expect(page).to have_button('Sign in with Google')
+    expect(page).to_not have_button('Sign Out')
+  end
+end
+
