5d5d0911c76a33c5db6a972c79075dc3af917b7f
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-03 11:53:51 -0600

    Create presenter instance variable to support view, pass search_params, whitelist with private method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   
   def index 
-    
+    @presenter = MovieSearchPresenter.new(search_params[:search])
+  end
+
+  private 
+
+  def search_params
+    params.permit(:search)
   end
 end
