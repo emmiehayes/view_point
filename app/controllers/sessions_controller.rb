7aea316cfee586063a388c6a79c67f8438b89c6d
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-02 20:10:11 -0600

    Add from_omniauth method to user model, create user in sessions controller and redirect to dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
 
-  def create 
+ def create
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:id] = user.id
+    redirect_to dashboard_path
   end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   validates_presence_of :email
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.first_name = auth.info.first_name.capitalize
+      user.token = auth.credentials.token 
+      user.refresh_token = auth.credentials.refresh_token
+      user.oauth_expires_at = auth.credentials.expires_at
+      user.save!
+    end
+  end
 end
