2ea4de3fd1d055c8e80d2828f69c4efab79fd00b
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-03 16:30:59 -0600

    Generate movie model

---
+++ db/migrate/20181103223021_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :summary
+      t.string :trailer_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :movie do
+    title { "MyString" }
+    summary { "MyString" }
+    trailer_url { "MyString" }
+  end
+end
