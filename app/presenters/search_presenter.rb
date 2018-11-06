f5aa6f76b90d2be323cba8f2247056ba8e355019
Emmie Hayes <emmiehayes2@gmail.com>
2018-11-06 16:52:15 -0700

    Adjust instance variables and create forecast method for view

---
+++ app/presenters/search_presenter.rb
@@ -0,0 +1,23 @@
+class SearchPresenter 
+
+  def initialize(search_params)
+    @movie_service = MovieService.new(search_params)
+    @weather_service = WeatherService.new(search_params)
+    @date = search_params[:date]
+  end
+
+  def movies 
+   movie_service.raw_movie_results.map do |result|
+      RecommendedMovie.new(result)
+    end
+  end
+
+  def forecast
+    weather_service.raw_weather_results.map do |result|
+      Condition.new(result)
+    end
+  end
+
+  private
+  attr_reader :movie_service, :weather_service
+end
